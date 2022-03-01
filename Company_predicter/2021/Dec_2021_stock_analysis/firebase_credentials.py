e254ed05e71967deadf811ccdb0cfa9deb46e7d5
tonykyo3232 <tonykyo3232@gmail.com>
2022-02-28 20:40:36 -0800

    remove the security token of data base

--- Company_predicter/2021/Dec_2021_stock_analysis/firebase_credentials.py
+++ Company_predicter/2021/Dec_2021_stock_analysis/firebase_credentials.py
@@ -1,10 +1,10 @@
 config = {
-  "apiKey": "AIzaSyCHRiXMyleftC-0aBbPxRsRvS5fJzdJLwQ",
-  "authDomain": "tweet-data-analysis.firebaseapp.com",
-  "projectId": "tweet-data-analysis",
-  "storageBucket": "tweet-data-analysis.appspot.com",
-  "messagingSenderId": "1054727249881",
-  "appId": "1:1054727249881:web:5bd4d4778d54ea37c2c3c8",
-  "measurementId": "G-ZKDSEPF00E",
+  "apiKey": "",
+  "authDomain": "",
+  "projectId": "",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": "",
+  "measurementId": "",
   "databaseURL": ""
 }
