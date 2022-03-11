c355a9508beef046921f9af54de1e955935fa75d
tonykyo3232 <tonykyo3232@gmail.com>
2022-03-10 18:42:05 -0800

    Version 2.0

---
+++ Company_predicter/2022/Feb/Feb_2022_stock_visualization/popular_twitter_accounts.py
@@ -0,0 +1,30 @@
+'''
+returns a dictionary of twitter account that discusses stock market
+# reference: https://www.investopedia.com/financial-edge/0712/10-twitter-feeds-investors-should-follow.aspx
+'''
+
+twitter_accounts = {
+	'Jim Cramer': 'jimcramer',
+	'CNBC': 'CNBC',
+	'Benzinga': 'Benzinga',
+	'Stocktwits': 'Stocktwits',
+	'Top Breakout Stocks': 'BreakoutStocks',
+	'Bespoke': 'bespokeinvest',
+	'WSJ Markets':'WSJmarkets',
+	'Stephanie Link':'Stephanie_Link',
+	'NYT Business':'nytimesbusiness',
+	'Investors.com':'IBDinvestors'
+	}
+
+account_names = [
+	'jimcramer',
+	'CNBC',
+	'Benzinga',
+	'Stocktwits',
+	'BreakoutStocks',
+	'bespokeinvest',
+	'WSJmarkets',
+	'Stephanie_Link',
+	'nytimesbusiness',
+	'IBDinvestors'
+]
